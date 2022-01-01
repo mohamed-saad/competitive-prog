079c4f821f2034fb8ba2aa2274f403fd63bbe798
Mohamed Saad <mohamed.m.saad@gmail.com>
2022-01-01 18:41:54 +0200

    112. Path Sum

---
+++ daily-challenge/src/main/java/competitive/leetcode/easy/tree/PathSum.java
@@ -0,0 +1,29 @@
+package competitive.leetcode.easy.tree;
+
+import competitive.leetcode.easy.tree.LevelOrder.TreeNode;
+
+public class PathSum {
+
+	private static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode() {}
+		TreeNode(int val) {this.val = val;}
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+	
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        if (root.right == null && root.left == null){ // leaf
+        	return (root.val == targetSum);
+        }
+        int remains = targetSum - root.val;
+        return root.left != null && hasPathSum(root.left, remains) || 
+        	   root.right != null && hasPathSum(root.right, remains);
+    }
+}
